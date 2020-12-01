464cd3ded8f69ef7a19497bc3ffb2ccac76f2b6b
CristianEtchebest <cristian.etchebest@meta.com.br>
2020-12-01 17:27:43 -0300

    #1 - Criado rota de exibição pokemom

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use GuzzleHttp\Client;
 Route::get('/', function () {
+
+    $pokemonBase = new Client([
+        'base_uri' => 'https://pokeapi.co/api/v2/pokemon/',
+        'timeout' => 2.0,
+    ]);
+
+    // Armazena o request no formato GET e com o parâmetro uri 1
+    $pokemonList = $pokemonBase->request('GET', '1');
+
+    //Retorna os dados em formato json
+    return json_decode($pokemonList->getBody(), true);
+
     return view('welcome');
 });
